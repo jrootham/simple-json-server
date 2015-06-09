e4a29e8c86655365f5a4331b42100c0aabc90c4b
jrootham <jrootham@gmail.com>
2015-06-09 18:39:25 -0400

    First code

---
+++ test/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+node test/testServer.js
+mocha test/mocha
---
+++ test/testServer.js
@@ -0,0 +1,12 @@
+/**
+ * testServer.js
+ *
+ * Created by jrootham on 25/04/15.
+ *
+ * Copyright © 2014 Jim Rootham
+ */
+(function () {
+    "use strict"
+
+    console.log("Hello, world");
+})()
